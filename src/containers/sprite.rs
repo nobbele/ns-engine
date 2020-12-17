0ff476da5b70d1ee2c27c0ed022744ceb58217a7
Nobbele <realnobbele@gmail.com>
2020-12-17 14:38:01 +0100

    adds drawparam mixing to sprite

--- src/containers/sprite.rs
+++ src/containers/sprite.rs
@@ -10,8 +10,16 @@ pub struct Sprite<T> {
 }
 
 impl<T: Drawable> Drawable for Sprite<T> {
-    fn draw(&self, ctx: &mut ggez::Context, _param: DrawParam) -> ggez::GameResult {
-        // TODO param mixing
-        self.content.draw(ctx, self.param)
+    fn draw(&self, ctx: &mut ggez::Context, param: DrawParam) -> ggez::GameResult {
+        let mut new_param = self.param;
+        new_param.color.r *= param.color.r;
+        new_param.color.g *= param.color.g;
+        new_param.color.b *= param.color.b;
+        new_param.color.a *= param.color.a;
+
+        new_param.scale.x *= param.scale.x;
+        new_param.scale.y *= param.scale.y;
+
+        self.content.draw(ctx, new_param)
     }
 }
