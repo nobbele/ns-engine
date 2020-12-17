3cd72f6a1314f41411a6ddfdecaaff7f4f37d9bf
Nobbele <realnobbele@gmail.com>
2020-12-17 15:37:07 +0100

    fixes character positioning

--- src/containers/character.rs
+++ src/containers/character.rs
@@ -21,30 +21,29 @@ impl Drawable for CharacterContainer {
     fn draw(&self, ctx: &mut Context, param: DrawParam) -> ggez::GameResult {
         for (n, character) in self.current.iter().enumerate() {
             let character = character.get_current();
-            let x_position = (drawable_size(ctx).0 as f32 / (self.current.len() as f32 + 1.0))
-                * (n as f32 + 1.0);
+
             let height = drawable_size(ctx).1 * (4.0 / 5.0);
-            let target_size: mint::Point2<f32> = [
-                height * (character.image.width() as f32 / character.image.height() as f32),
-                height,
-            ]
-            .into();
+            let target_size: mint::Point2<f32> = mint::Point2 {
+                x: height * (character.image.width() as f32 / character.image.height() as f32),
+                y: height,
+            };
+
+            let x_position = (drawable_size(ctx).0 as f32 / (self.current.len() as f32 + 1.0))
+                * (n as f32 + 1.0) - (target_size.x / 2.0);
+
             graphics::draw(
                 ctx,
                 &character.image,
-                graphics::DrawParam {
-                    dest: Position::BottomLeft.add_in(ctx, (x_position, target_size.y)),
-                    scale: [
-                        target_size.x / character.image.width() as f32,
-                        target_size.y / character.image.height() as f32,
-                    ]
-                    .into(),
-                    color: graphics::Color {
+                graphics::DrawParam::new()
+                    .dest(Position::BottomLeft.add_in(ctx, (x_position, target_size.y)))
+                    .scale(mint::Vector2 {
+                        x: target_size.x / character.image.width() as f32,
+                        y: target_size.y / character.image.height() as f32,
+                    })
+                    .color(graphics::Color {
                         a: character.alpha * param.color.a,
                         ..graphics::WHITE
-                    },
-                    ..Default::default()
-                },
+                    }),
             )?;
         }
         Ok(())
